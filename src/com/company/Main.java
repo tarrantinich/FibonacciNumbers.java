f0c2196d1c2c6ba443f5889df570d8b402986659
Сергей Покровский <pokrovsky.s.n@gmail.com>
2023-02-13 14:32:16 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,13 +12,17 @@ public class Main {
         int n1 = 0;
         int n2 = 1;
         int n3;
-        System.out.print(n1+" ");
-        System.out.print(n2+" ");
-        for (int i = 0; i < number - 2; i++) {
-            n3 = n1 + n2;
-            System.out.print(n3+" ");
-            n1 = n2;
-            n2 = n3;
+        if (number == 1){
+            System.out.print("0");
+        }else {
+            System.out.print(n1 + " ");
+            System.out.print(n2 + " ");
+            for (int i = 0; i < number - 2; i++) {
+                n3 = n1 + n2;
+                System.out.print(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
         }
     }
 }
